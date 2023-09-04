448ba1f2e2ee3925b285bc25a715901885172e72
Abhay <abhay.jha243@gmail.com>
2023-09-04 14:37:43 +0530

    Bug Fixes

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const corsOptions = {
 server.options('*', cors(corsOptions))
 
 server.get('/', (req, res) => {
-    res.send(path.join(__dirname, 'frontend/build/index.html'));
+    res.render(path.join(__dirname, 'frontend/build/index.html'));
 });
 
 server.post('/loginrequest', cors(corsOptions), (req, res, next) => {
